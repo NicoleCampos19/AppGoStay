19b05978697f0338eafc6b7563e1714f00c328cc
Emily <20230089@ricaldone.edu.sv>
2024-06-08 15:51:43 -0600

    Arreglo de las primeras interfaces

--- app/src/main/java/emily/jacobo/gostay/PrimerUsoActivity2.kt
+++ app/src/main/java/emily/jacobo/gostay/PrimerUsoActivity2.kt
@@ -10,7 +10,7 @@ class PrimerUsoActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_primer_uso2)
+        setContentView(R.layout.activity_bienvenida)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
