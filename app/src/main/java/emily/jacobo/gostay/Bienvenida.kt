cdab184f21bc057a9da2fe94cd786321072d0695
Emily <20230089@ricaldone.edu.sv>
2024-06-09 21:15:25 -0600

    Se inicio la navegabilidad y correcion de errores

--- app/src/main/java/emily/jacobo/gostay/Bienvenida.kt
+++ app/src/main/java/emily/jacobo/gostay/Bienvenida.kt
@@ -1,6 +1,8 @@
 package emily.jacobo.gostay
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,12 @@ class Bienvenida : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val btn = findViewById<Button>(R.id.btnContinuarBienvenida)
+
+        btn.setOnClickListener {
+            val siguientepantalla = Intent(this, activity_registrarse::class.java)
+            startActivity(siguientepantalla)
+        }
+
     }
 }
