6b5dde1ea344324244c87e53988761260033851d
gabriela <20190019@ricaldone.edu.sv>
2024-06-10 09:23:36 -0600

    actualizar rama y cambios

--- app/src/main/java/emily/jacobo/gostay/Bienvenida.kt
+++ app/src/main/java/emily/jacobo/gostay/Bienvenida.kt
@@ -1,6 +1,7 @@
 package emily.jacobo.gostay
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,6 @@ class Bienvenida : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     }
 }
