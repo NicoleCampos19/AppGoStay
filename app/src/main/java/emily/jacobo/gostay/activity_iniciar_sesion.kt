28151c43110af14d3345fc08988c4f0babd64f2f
20230268@ricaldone.edu.sv <20230268@ricaldone.edu.sv>
2024-06-17 12:13:44 -0600

    incriptacion

--- app/src/main/java/emily/jacobo/gostay/activity_iniciar_sesion.kt
+++ app/src/main/java/emily/jacobo/gostay/activity_iniciar_sesion.kt
@@ -9,6 +9,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.security.MessageDigest
 
 class activity_iniciar_sesion : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,12 +25,28 @@ class activity_iniciar_sesion : AppCompatActivity() {
             insets
         }
         val txtOlvidasteContrasena = findViewById<TextView>(R.id.txtOlvidasteContrasena)
+        val txtCorreoIniciarSesion = findViewById<TextView>(R.id.txtCorreoIniciarSesion)
+        val txtContrasenaIniciarSesion = findViewById<TextView>(R.id.txtContrasenaIniciarSesion)
         val imvAtrasc = findViewById<ImageView>(R.id.imvAtrasc)
         val btnIniciar = findViewById<Button>(R.id.btnIniciar)
+
+        fun hashSHA256(input: String): String {
+            val bytes = MessageDigest.getInstance("SHA-256").digest(input.toByteArray())
+            return bytes.joinToString("") { "%02x".format(it) }
+        }
 
         btnIniciar.setOnClickListener {
             val IniciarSesion = Intent(this, PaginaInicio::class.java)
             startActivity(IniciarSesion)
+
+            GlobalScope.launch(Dispatchers.IO) {
+
+                val objConexion = ClaseConexion().cadenaConexion()
+                //se manda la contrasena incriptada
+                val contraseniaEncriptada = hashSHA256(txtContrasenaIniciarSesion.text.toString())
+                    //aqui empieza la conexion con la base
+            }
+
         }
 
         txtOlvidasteContrasena.setOnClickListener {
--- app/src/main/java/emily/jacobo/gostay/activity_registrarse.kt
+++ app/src/main/java/emily/jacobo/gostay/activity_registrarse.kt
@@ -11,6 +11,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import emily.jacobo.gostay.R.id.txtIniciaSesion
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.security.MessageDigest
 
 class activity_registrarse : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -25,11 +29,32 @@ class activity_registrarse : AppCompatActivity() {
         }
         val imvAtrasc = findViewById<ImageView>(R.id.imvAtrasc)
         val txtIniciarsesion = findViewById<TextView>(R.id.txtIniciaSesion)
+        val txtNombre = findViewById<TextView>(R.id.txtNombre)
+        val txtApellido = findViewById<TxtView>(R.id.txtApellido)
+        val txtFechaNacimiento = findViewById<TextView>(R.id.txtFechaNacimiento)
+        val txtCorreoElectronico = findViewById<TextView>(R.id.txtCorreoElectronico)
+        val txtTelefono = findViewById<TextView>(R.id.txtTelefono)
+        val txtContrasena = findViewById<TextView>(R.id.txtContrasena)
         val btnRegistrarse = findViewById<Button>(R.id.btnRegistrarse)
+
+        fun hashSHA256(contraseniaEscrita: String): String {
+            val bytes = MessageDigest.getInstance("SHA-256").digest(contraseniaEscrita.toByteArray())
+            return bytes.joinToString("") { "%02x".format(it) }
+        }
 
         btnRegistrarse.setOnClickListener {
             val Registrarse = Intent(this, PaginaInicio::class.java)
             startActivity(Registrarse)
+
+            GlobalScope.launch(Dispatchers.IO) {
+
+                val objConexion = ClaseConexion().cadenaConexion()
+                    //se declara incriptada la contrasena para la base
+                val contraseniaEncriptada = hashSHA256(txtContrasena.text.toString())
+                // aqui abajo va la conexion a la base
+            }
+
+
         }
 
         txtIniciarsesion.setOnClickListener {
@@ -41,5 +66,8 @@ class activity_registrarse : AppCompatActivity() {
             val volverAtras = Intent(this, Bienvenida::class.java)
             startActivity(volverAtras)
         }
+
+
+
     }
 }
