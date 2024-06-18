707a964d4ca1661e20f83ec32f5fa954a25229dd
Emily <20230089@ricaldone.edu.sv>
2024-06-18 17:35:39 -0600

    Agregue las ip de todos

--- app/src/main/java/modelo/ClaseConexion.kt
+++ app/src/main/java/modelo/ClaseConexion.kt
@@ -6,12 +6,20 @@ import java.sql.DriverManager
 class ClaseConexion {
 
     fun cadenaConexion(): Connection?{
-        try {               //cambiar ip dependiendo de la computadora
-            val ip = "jdbc:oracle:thin:@10.10.1.119:1521:xe"
-            val usuario = "system"
-            val contrasena = "desarrollo"
+        try {
+            //cambiar ip dependiendo de la computadora
+            val ipLeonardo = "jdbc:oracle:thin:@192.168.0.11:1521:xe"
+            val ipEmily = "jdbc:oracle:thin:@192.168.0.13:1521:xe"
+            val ipGabriela = "jdbc:oracle:thin:@192.168.1.13:1521:xe"
+            val ipAbigail = "jdbc:oracle:thin:@192.168.0.8:1521:xe"
+            val ipSofia = "jdbc:oracle:thin:@172.24.192.1:1521:xe"
+            val ipDaniel = "jdbc:oracle:thin:@192.168.56.1:1521:xe"
 
-            val conexion = DriverManager.getConnection(ip, usuario, contrasena)
+            val usuario = "GoStay"
+            val contrasena = "ricaldone2024e"
+
+            //Conexión ip de Leo
+            val conexion = DriverManager.getConnection(ipLeonardo, usuario, contrasena)
             return conexion
         }catch (e: Exception){
             println("El error es este: $e")
