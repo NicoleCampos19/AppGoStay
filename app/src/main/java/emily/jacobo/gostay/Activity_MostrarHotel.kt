adb988a5c82ce832a5de0b6cb369955150761980
Daniel <messidaniel8620072@gmail.com>
2024-07-07 01:57:43 -0600

    hotel detalles y mostrar hotel

--- app/src/main/java/emily/jacobo/gostay/Activity_MostrarHotel.kt
+++ app/src/main/java/emily/jacobo/gostay/Activity_MostrarHotel.kt
@@ -58,7 +58,6 @@ class Activity_MostrarHotel : AppCompatActivity() {
         imgRegresarServiciosHotel.setOnClickListener {
             val intent = Intent(this, PaginaInicio::class.java)
             startActivity(intent)
-            overridePendingTransition(0, 0)
         }
     }
 }
