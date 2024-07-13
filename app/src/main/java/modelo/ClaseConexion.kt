f7bd79522115ccd3445760237dc47cc9353d36d1
DevLeonardo <20230070@ricaldone.edu.sv>
2024-07-13 13:30:39 -0600

    puse el item_tipohabitacion

--- app/src/main/java/modelo/ClaseConexion.kt
+++ app/src/main/java/modelo/ClaseConexion.kt
@@ -22,7 +22,7 @@ class ClaseConexion {
             val usuario = "GoStay"
             val contrasena = "ricaldone2024e"
 
-            val conexion = DriverManager.getConnection(ipGabriela, usuario, contrasena)
+            val conexion = DriverManager.getConnection(ipLeonardo, usuario, contrasena)
 
 
 
