108a7d1bf3e6bd490acd6872a029c1a534a4a62c
NicoleCampos19 <20230089@ricaldone.edu.sv>
2024-08-06 21:18:39 -0600

    Inicio y navegación en la parte de búsquedas

--- app/src/main/java/emily/jacobo/gostay/PaginaInicio.kt
+++ app/src/main/java/emily/jacobo/gostay/PaginaInicio.kt
@@ -4,6 +4,7 @@ import RecyclerViewHelpers.HotelAdapter
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -34,6 +35,13 @@ class PaginaInicio : AppCompatActivity() {
         val imvFavorito = findViewById<ImageView>(R.id.imvFavoritos)
         val imvReseva = findViewById<ImageView>(R.id.imvReservas)
         val imvPerfil = findViewById<ImageView>(R.id.imvPerfil)
+        val txtAggBusquedad = findViewById<TextView>(R.id.txtAggBusquedad)
+
+        txtAggBusquedad.setOnClickListener {
+            val siguientepantalla = Intent(this, opcionesdebusquedad::class.java)
+            startActivity(siguientepantalla)
+            overridePendingTransition(0, 0)
+        }
 
         imvBuscar.setOnClickListener {
             val siguientepantalla = Intent(this, PaginaInicio::class.java)
