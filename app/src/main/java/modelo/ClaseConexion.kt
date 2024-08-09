69c93062a3ef24d5c919394041cc7c79a697a568
Luz Gaspario <lmgaspariomendez@gmail.com>
2024-08-09 12:00:32 -0600

    Arrelo de vistas

--- app/src/main/java/modelo/ClaseConexion.kt
+++ app/src/main/java/modelo/ClaseConexion.kt
@@ -16,14 +16,14 @@ class ClaseConexion {
             val ipDaniel = "jdbc:oracle:thin:@192.168.56.1:1521:xe"
             val iprical = "jdbc:oracle:thin:@10.10.1.24:1521:xe"
             val ipprueba = "jdbc:oracle:thin:@10.10.0.51:1521:xe"
-            val ipBryan = "jdbc:oracle:thin:@172.24.160.1:1521:xe"
+            val ipBryan = "jdbc:oracle:thin:@10.10.3.42:1521:xe"
             val ipMirna = "jdbc:oracle:thin:@10.10.0.57:1521:xe"
             val ipDatosLeo = "jdbc:oracle:thin:@192.168.228.11:1521:xe"
 
             val usuario = "GoStay"
             val contrasena = "ricaldone2024e"
 
-            val conexion = DriverManager.getConnection(ipGabriela, usuario, contrasena)
+            val conexion = DriverManager.getConnection(ipBryan, usuario, contrasena)
 
 
 
