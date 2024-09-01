a43233138d6c71a13de52cda669cf59dc214a5eb
Daniel <messidaniel862007@gmail.com>
2024-09-01 12:03:52 -0600

    a

---
+++ app/src/main/java/RecyclerViewHelpers/AdaptadorCarrusel.kt
@@ -0,0 +1,32 @@
+package RecyclerViewHelpers
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import emily.jacobo.gostay.R
+
+class AdaptadorCarrusel(private val imageUrls: List<String>) : RecyclerView.Adapter<AdaptadorCarrusel.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.image_view)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_carrusel, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val imageUrl = imageUrls[position]
+        Glide.with(holder.itemView.context)
+            .load(imageUrl)
+            .into(holder.imageView)
+    }
+
+    override fun getItemCount(): Int {
+        return imageUrls.size
+    }
+}
