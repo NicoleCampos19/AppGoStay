6eaa47f9c7c09671241dfa59dc203a25a7e64dd4
Daniel <messidaniel862007@gmail.com>
2024-09-02 21:00:05 -0600

    casi nada del layout

--- app/src/main/java/modelo/ClaseConexion.kt
+++ app/src/main/java/modelo/ClaseConexion.kt
@@ -13,7 +13,7 @@ class ClaseConexion {
             val ipGabriela = "jdbc:oracle:thin:@192.168.0.100:1521:xe"
             val ipAbigail = "jdbc:oracle:thin:@172.20.10.3:1521:xe"
             val ipSofia = "jdbc:oracle:thin:@192.168.1.5:1521:xe"
-            val ipDaniel = "jdbc:oracle:thin:@192.168.56.1:1521:xe"
+            val ipDaniel = "jdbc:oracle:thin:@10.0.0.113:1521:xe"
             val iprical = "jdbc:oracle:thin:@ 10.10.1.120:1521:xe"
             val ipprueba = "jdbc:oracle:thin:@192.168.1.7:1521:xe"
             val ipBryan = "jdbc:oracle:thin:@192.168.56.1:1521:xe"
@@ -25,7 +25,7 @@ class ClaseConexion {
             val contrasena = "ricaldone2024e"
 
 
-            val conexion = DriverManager.getConnection(ipLeonardo, usuario, contrasena)
+            val conexion = DriverManager.getConnection(ipDaniel, usuario, contrasena)
 
 
             return conexion
