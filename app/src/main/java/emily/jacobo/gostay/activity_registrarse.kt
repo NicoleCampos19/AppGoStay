421cd1290d008eff987b44967beb9b4cef9f3ed4
NicolesCampos19 <20230089@ricaldone.edu.sv>
2024-09-04 23:40:36 -0600

    solo por un espacio xd

--- app/src/main/java/emily/jacobo/gostay/activity_registrarse.kt
+++ app/src/main/java/emily/jacobo/gostay/activity_registrarse.kt
@@ -226,6 +226,8 @@ class activity_registrarse : AppCompatActivity() {
                 setErrorWithCustomFont(txtConfirmarContraRegis, "Las contraseñas no coinciden", R.font.poppins)
                 hayErrores = true
             }
+
+
             // Si hay errores, no procede a guardar los datos
             if (hayVacios || hayErrores) {
                 Toast.makeText(this, "Verificar todos los campos", Toast.LENGTH_LONG)
