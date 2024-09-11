bd5fcdcb84be43f88dc42e715e9d763374f680ba
DevLeonardo <20230070@ricaldone.edu.sv>
2024-09-11 17:03:15 -0600

    Arreglo de lo que me faltaba

--- app/src/main/java/modelo/ClaseConexion.kt
+++ app/src/main/java/modelo/ClaseConexion.kt
@@ -23,7 +23,7 @@ class ClaseConexion {
            // val conexion = DriverManager.getConnection(ipDaniel, usuario, contrasena)
           //  val conexion = DriverManager.getConnection(ipGabriela, usuario, contrasena)
 
-            val conexion = DriverManager.getConnection(ipSofia, usuario, contrasena)
+            val conexion = DriverManager.getConnection(ipLeonardo, usuario, contrasena)
 
             return conexion
         }catch (e: Exception){
