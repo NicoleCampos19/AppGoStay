7fc0b023839830f63e4a3cccb38677c1f2de6bc8
DevLeonardo <20230070@ricaldone.edu.sv>
2024-09-13 20:02:36 -0600

    amor odio por las librerias, ya casi termiando el calendario

--- app/src/main/java/modelo/ClaseConexion.kt
+++ app/src/main/java/modelo/ClaseConexion.kt
@@ -24,7 +24,7 @@ class ClaseConexion {
            // val conexion = DriverManager.getConnection(ipDaniel, usuario, contrasena)
            // val conexion = DriverManager.getConnection(ipGabriela, usuario, contrasena)
 
-            val conexion = DriverManager.getConnection(ipprueba, usuario, contrasena)
+            val conexion = DriverManager.getConnection(ipLeonardo, usuario, contrasena)
 
             return conexion
         }catch (e: Exception){
